6082cff17d3ad3a1890486d991759fc583dfb636
Kyle Vanderlick <vanderpubell@gmail.com>
2026-09-13 17:01:00 -0400

    Deploy mengacci-v9

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * the version shows at the bottom of Setup so "which version is my phone on"
  * has an answer.
  */
-const CACHE = 'mengacci-v8';
+const CACHE = 'mengacci-v9';
 
 const SHELL = [
   './',
