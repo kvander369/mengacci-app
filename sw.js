936135997c7bc29c48b34ce4a0d52932e25b3d0b
Kyle Vanderlick <vanderpubell@gmail.com>
2026-09-13 17:27:49 -0400

    Deploy mengacci-v12

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * the version shows at the bottom of Setup so "which version is my phone on"
  * has an answer.
  */
-const CACHE = 'mengacci-v11';
+const CACHE = 'mengacci-v12';
 
 const SHELL = [
   './',
